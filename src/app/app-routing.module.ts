512935daa0563c6f5cc9f37bebe80a7ede163918
mindscan-de <maxim.gansert@gmail.com>
2020-11-22 11:32:21 +0100

    add initial main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {MainComponent} from './main/main.component';
+
+const routes: Routes = [
+	{ path: '', component:MainComponent},
+	{ path: 'main', component:MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
