89756e87acee8c536301f2bb60c44878cea9d78f
mindscan-de <maxim.gansert@gmail.com>
2020-11-23 20:56:28 +0100

    add uuid result type in the backend model.

---
+++ src/app/backend-services/backend-model/backend-model-uuidresult.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelUUIDResult } from './backend-model-uuidresult';
+
+describe('BackendModelUUIDResult', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelUUIDResult()).toBeTruthy();
+  });
+});
---
+++ src/app/backend-services/backend-model/backend-model-uuidresult.ts
@@ -0,0 +1,6 @@
+export class BackendModelUUIDResult {
+	public uuid: string = "";
+	
+	public isSuccess: boolean = false;
+	public isError: boolean = false;
+}
