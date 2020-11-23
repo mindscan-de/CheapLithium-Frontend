9e58932e14cd7d047925c7d92536a23ec7ec5814
mindscan-de <maxim.gansert@gmail.com>
2020-11-23 21:10:00 +0100

    add new backend urls to create a model or to update a decision model

--- src/app/backend-services/decision-model-backend.service.ts
+++ src/app/backend-services/decision-model-backend.service.ts
@@ -4,14 +4,16 @@ import { Observable } from 'rxjs/Observable';
 
 // include the BackendModel defintion
 import { BackendDecisionModel } from './backend-model/backend-decision-model';
-
+import { BackendModelUUIDResult } from './backend-model/backend-model-uuidresult';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DecisionModelBackendService {
 	
-	private _decisionModelLocation = '/CheapLithium/rest/getDecisionModel/';
+	private _decisionModelLocation          = '/CheapLithium/rest/getDecisionModel/';
+	private _createDecisionModelLocation    = '/CheapLithium/rest/createDecisionModel';
+	private _updateDecisionModelLocation    = '/CheapLithium/rest/updateDecisionModel';
 
 	constructor( private httpClient : HttpClient) { }
 
@@ -21,4 +23,26 @@ export class DecisionModelBackendService {
 		return this.httpClient.get<BackendDecisionModel>( this._decisionModelLocation + uuid, {params:httpParamteres} );
 	}
 
+	createDecisionModel(name:string, displayname:string, description:string, version:string) : Observable<BackendModelUUIDResult> {
+		let formdata = new FormData();
+		
+		formdata.append("name", name);
+		formdata.append("description", description);
+		formdata.append("displayname", displayname);
+		formdata.append("version", version);
+		
+		return this.httpClient.post<BackendModelUUIDResult>( this._createDecisionModelLocation, formdata);
+	}
+	
+	updateDecisionModel(uuid:string, name:string, displayname: string, description:string, version:string) : Observable<BackendModelUUIDResult> {
+		let formdata = new FormData();
+		
+		formdata.append("uuid", uuid);
+		formdata.append("name", name);
+		formdata.append("description", description);
+		formdata.append("displayname", displayname);
+		formdata.append("version", version);
+
+		return this.httpClient.post<BackendModelUUIDResult>( this._updateDecisionModelLocation, formdata);
+	}
 }
