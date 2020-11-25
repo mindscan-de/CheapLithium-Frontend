3c6083abc53c3208a440291d5fa2a97282dd5850
mindscan-de <maxim.gansert@gmail.com>
2020-11-25 22:28:35 +0100

    add dependencies for the modal dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,17 +8,23 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ShowDecisionModelComponent } from './show-decision-model/show-decision-model.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CreateDecisionModelDialogComponent } from './show-decision-model/create-decision-model-dialog/create-decision-model-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ShowDecisionModelComponent
+    ShowDecisionModelComponent ,
+	CreateDecisionModelDialogComponent
   ],
   imports: [
     BrowserModule,
 	HttpClientModule,
     AppRoutingModule,
+	FormsModule,
+	NgbModule,
     BrowserAnimationsModule
   ],
   providers: [],
