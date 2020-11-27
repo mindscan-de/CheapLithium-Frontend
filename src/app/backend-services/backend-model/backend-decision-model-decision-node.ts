4a5125efe373c3bbaca8284b200c5cd5565330e5
mindscan-de <maxim.gansert@gmail.com>
2020-11-27 18:27:21 +0100

    add names to the nodes - is useful for understanding the

--- src/app/backend-services/backend-model/backend-decision-model-decision-node.ts
+++ src/app/backend-services/backend-model/backend-decision-model-decision-node.ts
@@ -2,6 +2,7 @@ import {BackendDecisionModelDecisionNodeOutcome} from './backend-decision-model-
 
 export class BackendDecisionModelDecisionNode {
 	public uuid:string = "";
+	public name:string = "";
 	public type:string = "";
 	public kbarticle: string = "";
 	public nextActions: BackendDecisionModelDecisionNodeOutcome[]= []  
