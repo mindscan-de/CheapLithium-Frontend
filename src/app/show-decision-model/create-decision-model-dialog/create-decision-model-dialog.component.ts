f4534fb96782f9cb80fbdb83f07e646ea5cc4cad
mindscan-de <maxim.gansert@gmail.com>
2020-11-27 21:56:11 +0100

    add input fields to dialog and bind to model.

--- src/app/show-decision-model/create-decision-model-dialog/create-decision-model-dialog.component.ts
+++ src/app/show-decision-model/create-decision-model-dialog/create-decision-model-dialog.component.ts
@@ -13,11 +13,18 @@ import { BackendModelUUIDResult } from '../../backend-services/backend-model/bac
   styleUrls: ['./create-decision-model-dialog.component.css']
 })
 export class CreateDecisionModelDialogComponent {
+	
+	public dmName: string = "";
+	public dmDescription: string = "";
+	public dmVersion: string = "";
+	public dmDisplayName: string = "";
 
 	constructor(public activeModal: NgbActiveModal, private backendService : DecisionModelBackendService) { }
 	
 	onCreate() : void {
 		// Maybe we should pass an observable to the model back, with the BackendModelUUIDResult
+		
+		console.log( this.dmName  + ";" + this.dmDescription + ";" + this.dmVersion + ";" + this.dmDisplayName )
 		
 		this.activeModal.close('create');
 	}
