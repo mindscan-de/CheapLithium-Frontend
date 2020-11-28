6fda795e361e224084632ae0248b02ee5606a2a3
mindscan-de <maxim.gansert@gmail.com>
2020-11-28 21:46:04 +0100

    add some unfinished editor dialog ideas. skip that for now.

--- src/app/show-decision-model/edit-decision-node-dialog/edit-decision-node-dialog.component.ts
+++ src/app/show-decision-model/edit-decision-node-dialog/edit-decision-node-dialog.component.ts
@@ -6,6 +6,7 @@ import { DecisionModelBackendService } from '../../backend-services/decision-mod
 
 // Backend Model
 import { BackendModelUUIDResult } from '../../backend-services/backend-model/backend-model-uuidresult';
+import { BackendDecisionModelDecisionNode } from '../../backend-services/backend-model/backend-decision-model-decision-node';
 
 
 @Component({
@@ -14,11 +15,30 @@ import { BackendModelUUIDResult } from '../../backend-services/backend-model/bac
   styleUrls: ['./edit-decision-node-dialog.component.css']
 })
 export class EditDecisionNodeDialogComponent  {
+	
+	public dmUuid: string = "";
+	public decisionNode: BackendDecisionModelDecisionNode = new BackendDecisionModelDecisionNode();
 
 	constructor(public activeModal: NgbActiveModal, private backendService : DecisionModelBackendService) { }
 	
+	setDecisionNodeData(dmuuid: string, decisionNode: BackendDecisionModelDecisionNode) : void {
+		this.dmUuid = dmuuid;
+		this.decisionNode = decisionNode;
+	}
+	
+	onDeleteTransition( index:number ) : void {
+		// this.backendService.removeDecisionNodeTransition();
+		// then if successful, then adapt local list in decision node
+	}
+	
+	onCreateTransition() : void {
+		// maybe not yet...
+		// will not be implemented here now...
+		// open another modal dialog?
+	}
+	
 	onUpdate() : void {
-		
+		// go back to model uuid
 		this.activeModal.close('update');
 	}
 
