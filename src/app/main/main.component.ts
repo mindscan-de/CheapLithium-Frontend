17006139e949a15ad5cb4456abf27d609f8d50b6
mindscan-de <maxim.gansert@gmail.com>
2020-11-29 12:25:28 +0100

    read model index backend; show model index on main page; add toolbar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+// import the backend service, which provides the decision model data
+import { DecisionModelBackendService } from '../backend-services/decision-model-backend.service';
+import { BackendDecisionModelIndex } from '../backend-services/backend-model/backend-decision-model-index'; 
 
 @Component({
   selector: 'app-main',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+	
+	public decisionModelIndex: BackendDecisionModelIndex = new BackendDecisionModelIndex();
 
-  constructor() { }
+	constructor( private activatedRoute : ActivatedRoute, private backendService: DecisionModelBackendService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.backendService.getDecisionModelIndex().subscribe(
+			data => this.onDecisionModelIndexLoaded(data),
+			error => this.onDecisionModelIndexFailed(error)
+		);
+	}
+	
+	onDecisionModelIndexLoaded(index:BackendDecisionModelIndex) : void {
+		this.decisionModelIndex = index;
+	}
 
+	onDecisionModelIndexFailed(message) : void {
+		console.log(message);
+	}
+	
+	onClickDecisionModelShow(dmuuid:string) : void {
+		
+	}
+	
+	onClickDecisionModelExecute(dmuuid:string) : void {
+		
+	}
 }
