20678e734f8170f410502d040b8897e80385129e
mindscan-de <maxim.gansert@gmail.com>
2020-11-30 19:07:55 +0100

    add routing to the threads comonent / link the threads component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {MainComponent} from './main/main.component';
+import {ThreadsComponent} from './threads/threads.component';
 import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
 	{ path: 'main', component:MainComponent},
-	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent}
+	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
+	{ path: 'threads', component: ThreadsComponent}
 ];
 
 @NgModule({
