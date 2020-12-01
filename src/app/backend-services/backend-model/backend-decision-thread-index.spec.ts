a5893ea9b0f07f43636e9a1138d784af0a95d56a
mindscan-de <maxim.gansert@gmail.com>
2020-12-01 18:39:49 +0100

    start with a thread index backen model.

---
+++ src/app/backend-services/backend-model/backend-decision-thread-index.spec.ts
@@ -0,0 +1,7 @@
+import { BackendDecisionThreadIndex } from './backend-decision-thread-index';
+
+describe('BackendDecisionThreadIndex', () => {
+  it('should create an instance', () => {
+    expect(new BackendDecisionThreadIndex()).toBeTruthy();
+  });
+});
---
+++ src/app/backend-services/backend-model/backend-decision-thread-index.ts
@@ -0,0 +1,5 @@
+import { BackendDecisionThread } from './backend-decision-thread'
+
+export class BackendDecisionThreadIndex {
+	public threads: BackendDecisionThread[] = []
+}
