ade6f50647f5a511629d26ae3fd59bcb92553b91
mindscan-de <maxim.gansert@gmail.com>
2020-12-01 18:43:57 +0100

    implement a first base of the decision thread backend service

--- src/app/backend-services/decision-thread-backend.service.ts
+++ src/app/backend-services/decision-thread-backend.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs/Observable';
+
+import { BackendDecisionThread } from './backend-model/backend-decision-thread';
+import { BackendDecisionThreadIndex } from './backend-model/backend-decision-thread-index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DecisionThreadBackendService {
+	
+	private _decisionThreadLocation          = '/CheapLithium/rest/getDecisionThread/';
+	private _decisionThreadListLocation      = '/CheapLithium/rest/getDecisionThreadList';
+	
 
-  constructor() { }
+	constructor( private httpClient : HttpClient) { }
+
+	getDecisionThread ( uuid : string ): Observable<BackendDecisionThread> {
+		let httpParameters = new HttpParams();
+		
+		return this.httpClient.get<BackendDecisionThread>(this._decisionThreadLocation + uuid, {params:httpParameters});
+	}
+	
+	getDecisionThreadIndex() : Observable<BackendDecisionThreadIndex> {
+		let httpParameters = new HttpParams();
+		
+		return this.httpClient.get<BackendDecisionThreadIndex>( this._decisionThreadListLocation, {params:httpParameters});
+	}
 }
