43de02a9af34cd85d2e52583aeb262c4131d4a94
mindscan-de <maxim.gansert@gmail.com>
2020-12-01 18:57:07 +0100

    read the index, on retrieving/initializing the threads component

--- src/app/threads/threads.component.ts
+++ src/app/threads/threads.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+import { DecisionThreadBackendService }  from '../backend-services/decision-thread-backend.service';
+
+
+import { BackendDecisionThreadIndex } from '../backend-services/backend-model/backend-decision-thread-index';
 
 @Component({
   selector: 'app-threads',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./threads.component.css']
 })
 export class ThreadsComponent implements OnInit {
+	
+	public decisionThreadIndex: BackendDecisionThreadIndex = new BackendDecisionThreadIndex();
 
-  constructor() { }
+	constructor( private activatedRoute : ActivatedRoute, private backendService: DecisionThreadBackendService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.backendService.getDecisionThreadIndex().subscribe(
+			data => this.onDecisionThreadIndexLoaded(data),
+			error => this.onDecisionThreadIndexFailed(error)
+		);
+	}
 
+	onDecisionThreadIndexLoaded( index: BackendDecisionThreadIndex):void {
+		this.decisionThreadIndex = index;
+	}
+	
+	onDecisionThreadIndexFailed(message):void {
+		console.log(message);
+	}
+	
 }
