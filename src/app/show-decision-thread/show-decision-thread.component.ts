70087b9bc0cc5d06f068d839d225a117a0e8ec91
mindscan-de <maxim.gansert@gmail.com>
2020-12-02 22:54:45 +0100

    add some data to the show decision thread page.

--- src/app/show-decision-thread/show-decision-thread.component.ts
+++ src/app/show-decision-thread/show-decision-thread.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+import { BackendDecisionModel } from '../backend-services/backend-model/backend-decision-model';
+import { BackendDecisionModelDecisionNode } from '../backend-services/backend-model/backend-decision-model-decision-node';
+import { BackendDecisionThread } from '../backend-services/backend-model/backend-decision-thread';
+
+// import the backend service, which provides the decision model data
+import { DecisionModelBackendService } from '../backend-services/decision-model-backend.service';
+import { DecisionThreadBackendService } from '../backend-services/decision-thread-backend.service';
 
 @Component({
   selector: 'app-show-decision-thread',
@@ -6,10 +17,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-decision-thread.component.css']
 })
 export class ShowDecisionThreadComponent implements OnInit {
+	
+	public decisionThread: BackendDecisionThread = new BackendDecisionThread();
+	public decisionThreadTmp: BackendDecisionThread; 
+	public decisionModel: BackendDecisionModel = new BackendDecisionModel();
+	public decisionNodeMap: Map<string,BackendDecisionModelDecisionNode> = new Map(); 
 
-  constructor() { }
+	constructor( private activatedRoute : ActivatedRoute, private backendModelService: DecisionModelBackendService, private backendThreadService: DecisionThreadBackendService,  private modalService: NgbModal) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		var threaduuid = this.activatedRoute.snapshot.params['uuid'];
+		
+		this.retrieveThread(threaduuid);
+	}
+	
+	retrieveThread(threaduuid: string) : void {
+		this.backendThreadService.getDecisionThread(threaduuid).subscribe(
+			data => this.onDecisionThreadLoaded(data),
+			error => this.onDecisionThreadFailed(error)
+		);
+	}
+	
+    onDecisionThreadLoaded(thread: BackendDecisionThread) : void {
+		console.log(thread);
+		this.backendModelService.getDecisionModel(thread.currentmodel).subscribe(
+			data => this.onDecisionModelLoaded(data),
+			error => this.onDecisionModelFailed(error)
+		);
+
+		// TODO read the runtime data(runtume environment) / subscribe to the runtime data
+		
+		this.decisionThreadTmp = thread;
+    }
+
+	onDecisionModelLoaded( model:BackendDecisionModel ): void {
+		var newMap = new Map<string, BackendDecisionModelDecisionNode>();
+		
+		model.nodes.forEach( function (node) {
+			newMap.set(node.uuid, node);
+		});
+		
+		this.decisionNodeMap = newMap;
+		this.decisionModel = model;
+		this.decisionThread = this.decisionThreadTmp;
+	}
+
+	
+    onDecisionThreadFailed(error: any) : void {
+        console.log(error);
+    }
+
+	onDecisionModelFailed( error:any ) : void {
+		console.log(error);
+	}
+
 
 }
