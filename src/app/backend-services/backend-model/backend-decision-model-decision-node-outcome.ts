c05a3bc897ceb96c6123f3dae27866e0e6af600d
mindscan-de <maxim.gansert@gmail.com>
2020-12-03 17:55:31 +0100

    add reminders to the backend type to provide simple compute capabilities for decision nodes and transitions.

--- src/app/backend-services/backend-model/backend-decision-model-decision-node-outcome.ts
+++ src/app/backend-services/backend-model/backend-decision-model-decision-node-outcome.ts
@@ -5,4 +5,9 @@ export class BackendDecisionModelDecisionNodeOutcome {
 	public next: string = "";
 	
 	public template: string = "";
+	
+	// one of the operations must return true, then the model will proceed automatically
+	// with the correct transition, is most likely to be a boolean expression
+	// part of the upcomming workflow engine stuff, so things can be done automatically.
+	public isTransition: string = "";
 }
--- src/app/backend-services/backend-model/backend-decision-model-decision-node.ts
+++ src/app/backend-services/backend-model/backend-decision-model-decision-node.ts
@@ -5,5 +5,13 @@ export class BackendDecisionModelDecisionNode {
 	public name:string = "";
 	public type:string = "";
 	public kbarticle: string = "";
-	public nextactions: BackendDecisionModelDecisionNodeOutcome[]= []  
+	public nextactions: BackendDecisionModelDecisionNodeOutcome[]= []
+	
+	// TODO:
+	// those both things will be filled so we can run nonhumantasks too.
+	// this will describe the operation(s) to do at this node, and what to do with the result
+	public operation:string = "";
+	// 
+	public result:string = "";
+
 }
