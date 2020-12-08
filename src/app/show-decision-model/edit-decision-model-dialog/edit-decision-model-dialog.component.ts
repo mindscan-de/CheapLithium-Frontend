39d266e67901a010ae79d721b1bf171b37bcf2d1
mindscan-de <maxim.gansert@gmail.com>
2020-12-08 21:28:11 +0100

    inject data into editor dialog and return new data from dialog

--- src/app/show-decision-model/edit-decision-model-dialog/edit-decision-model-dialog.component.ts
+++ src/app/show-decision-model/edit-decision-model-dialog/edit-decision-model-dialog.component.ts
@@ -3,6 +3,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 // Backend Model
 // TODO: add the Model component 
+import {BackendDecisionModel} from '../../backend-services/backend-model/backend-decision-model';
 
 
 @Component({
@@ -11,22 +12,32 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./edit-decision-model-dialog.component.css']
 })
 export class EditDecisionModelDialogComponent  {
+
 	
 	public dmName: string = "";
 	public dmDescription: string = "";
 	public dmVersion: string = "";
 	public dmDisplayName: string = "";
+	
 
 	constructor(public activeModal: NgbActiveModal ) { }
 
-	setDialogData() : void {
-		
+	setDialogData( decisionModel:BackendDecisionModel ) : void {
+		this.dmName = decisionModel.name;
+		this.dmDisplayName = decisionModel.displayname;
+		this.dmVersion = decisionModel.version;
+		this.dmDescription = decisionModel.description;
 	}
 
 	onUpdate() : void {
-		// Maybe we should pass an observable to the model back, with the BackendModelUUIDResult
+		let newDecisionModelValues:BackendDecisionModel = new BackendDecisionModel(); 
 		
-		this.activeModal.close('update');
+		newDecisionModelValues.description = this.dmDescription;
+		newDecisionModelValues.displayname = this.dmDisplayName;
+		newDecisionModelValues.name = this.dmName;
+		newDecisionModelValues.version = this.dmVersion;
+		
+		this.activeModal.close(newDecisionModelValues);
 	}
 
 }
