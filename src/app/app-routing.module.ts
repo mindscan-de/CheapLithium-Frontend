97df2e65a49be52538edac7487acc6e62d2c56fe
mindscan-de <maxim.gansert@gmail.com>
2020-12-08 20:23:54 +0100

    integrate the empty knowledge base component into application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {MainComponent} from './main/main.component';
 import {ThreadsComponent} from './threads/threads.component';
+import {KbaseComponent} from './kbase/kbase.component';
 import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
 import {ShowDecisionThreadComponent} from './show-decision-thread/show-decision-thread.component';
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
 	{ path: 'main', component:MainComponent},
+	{ path: 'kbase', component:KbaseComponent},
 	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
 	{ path: 'threads', component: ThreadsComponent},
 	{ path: 'showDecisionThread/:uuid', component: ShowDecisionThreadComponent}	
