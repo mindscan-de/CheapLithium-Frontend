451d15ae89aba8224c65fc0923248b41e14b4de5
mindscan-de <maxim.gansert@gmail.com>
2020-12-09 21:11:43 +0100

    add new backed thread uuid result type

---
+++ src/app/backend-services/backend-model/backend-thread-uuidresult.spec.ts
@@ -0,0 +1,7 @@
+import { BackendThreadUUIDResult } from './backend-thread-uuidresult';
+
+describe('BackendThreadUUIDResult', () => {
+  it('should create an instance', () => {
+    expect(new BackendThreadUUIDResult()).toBeTruthy();
+  });
+});
---
+++ src/app/backend-services/backend-model/backend-thread-uuidresult.ts
@@ -0,0 +1,7 @@
+export class BackendThreadUUIDResult {
+	public uuid: string = "";
+	
+	public isSuccess: boolean = false;
+	public isError: boolean = false;
+
+}
