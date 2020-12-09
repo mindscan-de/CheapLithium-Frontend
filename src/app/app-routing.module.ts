1ae67b000b82d5196ffcf99616994f5da4cf1445
mindscan-de <maxim.gansert@gmail.com>
2020-12-09 20:45:53 +0100

    use the new application routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {MainComponent} from './main/main.component';
 import {ThreadsComponent} from './threads/threads.component';
 import {KbaseComponent} from './kbase/kbase.component';
+import {ModelsComponent} from './models/models.component';
 import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
 import {ShowDecisionThreadComponent} from './show-decision-thread/show-decision-thread.component';
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
 	{ path: 'main', component:MainComponent},
-	{ path: 'models', component:MainComponent},
+	{ path: 'models', component:ModelsComponent},
 	{ path: 'kbase', component:KbaseComponent},
 	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
 	{ path: 'threads', component: ThreadsComponent},
