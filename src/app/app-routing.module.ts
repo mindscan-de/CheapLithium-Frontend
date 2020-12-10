1a553417ec9fb3dfe5d9ed97270fa8805a7086ab
mindscan-de <maxim.gansert@gmail.com>
2020-12-10 20:31:34 +0100

    implement the router configuration for showing an article

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import {KbaseComponent} from './kbase/kbase.component';
 import {ModelsComponent} from './models/models.component';
 import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
 import {ShowDecisionThreadComponent} from './show-decision-thread/show-decision-thread.component';
+import {ShowKBArticleComponent} from './show-kb-article/show-kb-article.component';
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
 	{ path: 'main', component:MainComponent},
 	{ path: 'models', component:ModelsComponent},
 	{ path: 'kbase', component:KbaseComponent},
+	{ path: 'showArticle/:uuid', component: ShowKBArticleComponent },
 	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
 	{ path: 'threads', component: ThreadsComponent},
 	{ path: 'showDecisionThread/:uuid', component: ShowDecisionThreadComponent}	
