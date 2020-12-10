52796cf082f60df5df5a5335427dcc8d16bf1896
mindscan-de <maxim.gansert@gmail.com>
2020-12-10 17:50:49 +0100

    add start a decision model url to backend service.

--- src/app/backend-services/decision-thread-backend.service.ts
+++ src/app/backend-services/decision-thread-backend.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs/Observable';
 
 import { BackendDecisionThread } from './backend-model/backend-decision-thread';
 import { BackendDecisionThreadIndex } from './backend-model/backend-decision-thread-index';
+import { BackendThreadUUIDResult } from './backend-model/backend-thread-uuidresult';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,10 @@ export class DecisionThreadBackendService {
 	
 	private _decisionThreadLocation          = '/CheapLithium/rest/getDecisionThread/';
 	private _decisionThreadListLocation      = '/CheapLithium/rest/getDecisionThreadList';
+	private _decisionThreadStartLocation     = '/CheapLithium/rest/startDecisionThread';
+	
+	// TODO: this is the reporting Backend URL - which converts a thread into a report, using the logged transitions
+	private _decisionThreadReportLocation    = '/CheapLithium/rest/getDecisionThreadReport/';
 	
 
 	constructor( private httpClient : HttpClient) { }
@@ -26,5 +32,16 @@ export class DecisionThreadBackendService {
 		let httpParameters = new HttpParams();
 		
 		return this.httpClient.get<BackendDecisionThreadIndex>( this._decisionThreadListLocation, {params:httpParameters});
+	}
+	
+	startDecisionThread ( uuid: string, ticketreference:string ):Observable<BackendThreadUUIDResult> {
+		let httpParameters = new HttpParams();
+		
+		// what
+		httpParameters.append("uuid", uuid);
+		httpParameters.append("ticketreference", ticketreference)
+		// add more data?
+		
+		return this.httpClient.post<BackendThreadUUIDResult>(this._decisionThreadStartLocation, {params:httpParameters});
 	}
 }
