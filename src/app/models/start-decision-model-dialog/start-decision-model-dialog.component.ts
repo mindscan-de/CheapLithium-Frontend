8b0b510af598277962338ba0280e6a29a512b88a
mindscan-de <maxim.gansert@gmail.com>
2020-12-10 17:53:15 +0100

    improve start dialog to start a decision model

--- src/app/models/start-decision-model-dialog/start-decision-model-dialog.component.ts
+++ src/app/models/start-decision-model-dialog/start-decision-model-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { BackendDecisionModel } from '../../backend-services/backend-model/backend-decision-model';
+
 // backend Service
 import { DecisionModelBackendService } from '../../backend-services/decision-model-backend.service';
 
@@ -11,11 +13,19 @@ import { DecisionModelBackendService } from '../../backend-services/decision-mod
   styleUrls: ['./start-decision-model-dialog.component.css']
 })
 export class StartDecisionModelDialogComponent  {
+	
+    public decisionModel: BackendDecisionModel = new BackendDecisionModel();
+	public ticketReference : string = ""; 
 
 	constructor(public activeModal: NgbActiveModal, private backendService : DecisionModelBackendService) { }
+	
+	setDialogData(decisionModel:BackendDecisionModel) : void {
+		this.decisionModel = decisionModel;
+	}
 
 	onRun() : void {
 		// this dialog should run the model and let 
+		
 		
 		this.activeModal.close('run');
 	}
