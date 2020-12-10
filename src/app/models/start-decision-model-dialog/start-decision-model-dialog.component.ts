8a29d747e901d6e407e49f433e2c795c31620c71
mindscan-de <maxim.gansert@gmail.com>
2020-12-10 18:24:12 +0100

    extend the start dialog and invoke start method from backend

--- src/app/models/start-decision-model-dialog/start-decision-model-dialog.component.ts
+++ src/app/models/start-decision-model-dialog/start-decision-model-dialog.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { BackendDecisionModel } from '../../backend-services/backend-model/backend-decision-model';
+import { BackendThreadUUIDResult } from '../../backend-services/backend-model/backend-thread-uuidresult';
 
 // backend Service
 import { DecisionModelBackendService } from '../../backend-services/decision-model-backend.service';
+import { DecisionThreadBackendService } from '../../backend-services/decision-thread-backend.service';
 
 
 @Component({
@@ -17,16 +19,29 @@ export class StartDecisionModelDialogComponent  {
     public decisionModel: BackendDecisionModel = new BackendDecisionModel();
 	public ticketReference : string = ""; 
 
-	constructor(public activeModal: NgbActiveModal, private backendService : DecisionModelBackendService) { }
+	constructor(public activeModal: NgbActiveModal, private backendService : DecisionThreadBackendService) { }
 	
 	setDialogData(decisionModel:BackendDecisionModel) : void {
 		this.decisionModel = decisionModel;
 	}
 
 	onRun() : void {
-		// this dialog should run the model and let 
+		// this dialog should run the model and let
+		this.backendService.startDecisionThread( this.decisionModel.uuid, this.ticketReference).subscribe(
+			data => this.onUUIDResult(data),
+			error => this.onError(error)
+		)
+	}
+	
+	
+	onUUIDResult(threadUUIDResult:BackendThreadUUIDResult):void {
+		// TODO: if success cool, and leave dialog)
+		// TODO: id nonsuccessfull uuid, then do something impressing
 		
-		
-		this.activeModal.close('run');
+		this.activeModal.close(threadUUIDResult);
+	}
+	
+	onError(error:any) : void {
+		console.log(error);
 	}
 }
