84db537ddbbc3590782a5b7fbbb6bd94a16faf89
mindscan-de <maxim.gansert@gmail.com>
2020-12-10 20:31:10 +0100

    implement showing the pagetitle of the article.

--- src/app/show-kb-article/show-kb-article.component.ts
+++ src/app/show-kb-article/show-kb-article.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BackendKBArticle } from '../backend-services/backend-model/backend-kb-article';
+
 @Component({
   selector: 'app-show-kb-article',
   templateUrl: './show-kb-article.component.html',
   styleUrls: ['./show-kb-article.component.css']
 })
 export class ShowKBArticleComponent implements OnInit {
+	
+	public article:BackendKBArticle = new BackendKBArticle();
 
   constructor() { }
 
