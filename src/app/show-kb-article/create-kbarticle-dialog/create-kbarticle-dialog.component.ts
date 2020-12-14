57a07feaef30108df8bca69a97592532768293fd
mindscan-de <maxim.gansert@gmail.com>
2020-12-14 18:37:46 +0100

    implement the dialog logic for creating a new knowledge base article

--- src/app/show-kb-article/create-kbarticle-dialog/create-kbarticle-dialog.component.ts
+++ src/app/show-kb-article/create-kbarticle-dialog/create-kbarticle-dialog.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { KnowledgeBaseBackendService } from '../../backend-services/knowledge-base-backend.service'
 
 @Component({
   selector: 'app-create-kbarticle-dialog',
   templateUrl: './create-kbarticle-dialog.component.html',
   styleUrls: ['./create-kbarticle-dialog.component.css']
 })
-export class CreateKBArticleDialogComponent implements OnInit {
+export class CreateKBArticleDialogComponent  {
+	
+	public kbaPageTitle : string = "";
+	public kbaPageSummary : string = "";
+	public kbaPageContent : string = "";
 
-  constructor() { }
+	constructor(public activeModal: NgbActiveModal, private backendService : KnowledgeBaseBackendService ) {}
 
-  ngOnInit(): void {
-  }
-
+	onCreate() : void {
+		// TODO use the backendservice to create / if success then close the modalwith the result.
+		// FOR NOW we will stick with returning the observable
+		
+		this.activeModal.close('done')
+	}
 }
--- src/app/show-kb-article/show-kb-article.component.ts
+++ src/app/show-kb-article/show-kb-article.component.ts
@@ -6,6 +6,9 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 // import the backend service.
 import { KnowledgeBaseBackendService } from '../backend-services/knowledge-base-backend.service';
 
+// import dialogs.
+import { CreateKBArticleDialogComponent } from "./create-kbarticle-dialog/create-kbarticle-dialog.component";
+
 // import the backend model data types
 import { BackendKBArticle } from '../backend-services/backend-model/backend-kb-article';
 import { BackendModelUUIDResult } from '../backend-services/backend-model/backend-model-uuidresult';
@@ -19,7 +22,7 @@ export class ShowKBArticleComponent implements OnInit {
 	
 	public article:BackendKBArticle = new BackendKBArticle();
 
-	constructor(private activatedRoute : ActivatedRoute, private backendService: KnowledgeBaseBackendService, private modalService: NgbModal) { }
+	constructor(private activatedRoute : ActivatedRoute, private backendService: KnowledgeBaseBackendService, private modalService: NgbModal, private router : Router) { }
 
 	ngOnInit(): void {
 		var uuid = this.activatedRoute.snapshot.params['uuid'];
@@ -47,11 +50,31 @@ export class ShowKBArticleComponent implements OnInit {
 		// This will create a new article and the user shoud then be redirectedto the new article,
 		// the dialog will create it and then return a new uuid
 		// then the angular router will redirect the user to the new url with the new uuid
+		const modalref = this.modalService.open(CreateKBArticleDialogComponent, {centered: true, ariaLabelledBy: 'modal-basic-title', size:'xl' })
+		
+		modalref.result.then((result) => {
+			result.subscribe(
+				data => this.onUUIDResult(data),
+				error => this.onError(error)
+			)
+		}, (reason) => {
+		  // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+		});
 	}
 	
 	onEditArticle() : void {
 		// this will open a dialog for editing an article, the user is then redicted to the same 
 		// article again / reload the content using the retrieveArticle method. 
+	}
+
+
+	onUUIDResult(result:BackendModelUUIDResult) : void {
+		this.router.navigate(['/showArticle', {uuid:result.uuid}]);
+	}
+
+
+	onError(error):void {
+		console.log(error)
 	}
 
 }
