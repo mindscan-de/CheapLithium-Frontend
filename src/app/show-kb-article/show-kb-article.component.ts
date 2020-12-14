9ea3043d4e6831bc4947802b007c76c4f569e655
mindscan-de <maxim.gansert@gmail.com>
2020-12-14 12:46:15 +0100

    show the knowledge base article.

--- src/app/show-kb-article/show-kb-article.component.ts
+++ src/app/show-kb-article/show-kb-article.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+// import the backend service.
+import { KnowledgeBaseBackendService } from '../backend-services/knowledge-base-backend.service';
+
+// import the backend model data types
 import { BackendKBArticle } from '../backend-services/backend-model/backend-kb-article';
+import { BackendModelUUIDResult } from '../backend-services/backend-model/backend-model-uuidresult';
 
 @Component({
   selector: 'app-show-kb-article',
@@ -11,9 +19,29 @@ export class ShowKBArticleComponent implements OnInit {
 	
 	public article:BackendKBArticle = new BackendKBArticle();
 
-  constructor() { }
+	constructor(private activatedRoute : ActivatedRoute, private backendService: KnowledgeBaseBackendService, private modalService: NgbModal) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		var uuid = this.activatedRoute.snapshot.params['uuid'];
+		
+		this.retrieveArticle(uuid);
+	}
+	
+	
+    retrieveArticle(uuid: any) {
+        this.backendService.getKBArticle(uuid).subscribe(
+			data => this.onArticleLoaded(data),
+			error => this.onArticleFailed(error)
+		);
+    }
+
+    onArticleLoaded(article: BackendKBArticle): void {
+        this.article = article;
+    }
+
+    onArticleFailed(error: any): void {
+        console.log(error);
+    }
+
 
 }
