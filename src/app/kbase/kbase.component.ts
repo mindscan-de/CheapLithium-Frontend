2d42c91dee9db0cff06727bbfeba298d2bd0e231
mindscan-de <maxim.gansert@gmail.com>
2020-12-14 12:11:40 +0100

    show the list of knowledge base articles found.

--- src/app/kbase/kbase.component.ts
+++ src/app/kbase/kbase.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+import { KnowledgeBaseBackendService } from '../backend-services/knowledge-base-backend.service';
+import { BackendKBArticle } from '../backend-services/backend-model/backend-kb-article';
+import { BackendKBArticleIndex } from '../backend-services/backend-model/backend-kb-article-index';
 
 @Component({
   selector: 'app-kbase',
@@ -6,10 +13,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kbase.component.css']
 })
 export class KbaseComponent implements OnInit {
+	
+	public kbarticleIndex: BackendKBArticleIndex = new BackendKBArticleIndex(); 
 
-  constructor() { }
+	constructor(private activatedRoute : ActivatedRoute, private backendService: KnowledgeBaseBackendService, private modalService: NgbModal, private router : Router) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.backendService.getKBArticleList().subscribe(
+			data => this.onKnowledgebaseLoaded(data),
+			error => this.onKnowledgebaseFailed(error)
+		);
+	}
+    onKnowledgebaseFailed(message: any): void {
+        console.log(message);
+    }
+
+    onKnowledgebaseLoaded(index: BackendKBArticleIndex): void {
+		this.kbarticleIndex = index
+    }
 
 }
