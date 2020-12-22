df5bb5e0bd339ccfe26c41f82839d010b9662b78
mindscan-de <maxim.gansert@gmail.com>
2020-12-22 20:43:14 +0100

    add application routing to present a decision thread report

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {ModelsComponent} from './models/models.component';
 import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
 import {ShowDecisionThreadComponent} from './show-decision-thread/show-decision-thread.component';
 import {ShowKBArticleComponent} from './show-kb-article/show-kb-article.component';
+import {ShowDecisionThreadReportComponent} from './show-decision-thread-report/show-decision-thread-report.component';
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
@@ -17,7 +18,8 @@ const routes: Routes = [
 	{ path: 'showArticle/:uuid', component: ShowKBArticleComponent },
 	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
 	{ path: 'threads', component: ThreadsComponent},
-	{ path: 'showDecisionThread/:uuid', component: ShowDecisionThreadComponent}	
+	{ path: 'showDecisionThread/:uuid', component: ShowDecisionThreadComponent},
+	{ path: 'showDecisionThreadReport/:uuid', component: ShowDecisionThreadReportComponent}	
 ];
 
 @NgModule({
