ee7cf8192a2cb95639f80f436b432f3d187d45d1
mindscan-de <maxim.gansert@gmail.com>
2020-12-24 13:15:29 +0100

    add additional data provided by the backend to the backend class.

--- src/app/backend-services/backend-model/backend-decision-thread-report-item.ts
+++ src/app/backend-services/backend-model/backend-decision-thread-report-item.ts
@@ -1,6 +1,10 @@
 export class BackendDecisionThreadReportItem {
 	public nodereport: string = "";
+	public modelname: string = "";
+	public modeluuid: string = "";
+	public transitionname: string = "";
 	public nodename:string = "";
+	public nodeuuid:string = "";
 	public timestamp:string = "";
 	public data: Map<string,any> = new Map<string,any>();
 }
