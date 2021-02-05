95f341703fd60530a0d297f9bb2670fc447d9868
mindscan-de <maxim.gansert@gmail.com>
2021-02-05 20:01:29 +0100

    integrate show decision thread errors into routing and other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {MainComponent} from './main/main.component';
-import {ThreadsComponent} from './threads/threads.component';
-import {KbaseComponent} from './kbase/kbase.component';
-import {ModelsComponent} from './models/models.component';
-import {ShowDecisionModelComponent} from './show-decision-model/show-decision-model.component'
-import {ShowDecisionThreadComponent} from './show-decision-thread/show-decision-thread.component';
-import {ShowKBArticleComponent} from './show-kb-article/show-kb-article.component';
-import {ShowDecisionThreadReportComponent} from './show-decision-thread-report/show-decision-thread-report.component';
+import { MainComponent } from './main/main.component';
+import { ThreadsComponent } from './threads/threads.component';
+import { KbaseComponent } from './kbase/kbase.component';
+import { ModelsComponent } from './models/models.component';
+import { ShowDecisionModelComponent } from './show-decision-model/show-decision-model.component'
+import { ShowDecisionThreadComponent } from './show-decision-thread/show-decision-thread.component';
+import { ShowKBArticleComponent } from './show-kb-article/show-kb-article.component';
+import { ShowDecisionThreadReportComponent } from './show-decision-thread-report/show-decision-thread-report.component';
+import { ShowDecisionThreadErrorsComponent } from './show-decision-thread-errors/show-decision-thread-errors.component';
 
 const routes: Routes = [
 	{ path: '', component:MainComponent},
@@ -19,7 +20,8 @@ const routes: Routes = [
 	{ path: 'showDecisionModel/:uuid', component: ShowDecisionModelComponent},
 	{ path: 'threads', component: ThreadsComponent},
 	{ path: 'showDecisionThread/:uuid', component: ShowDecisionThreadComponent},
-	{ path: 'showDecisionThreadReport/:uuid', component: ShowDecisionThreadReportComponent}	
+	{ path: 'showDecisionThreadReport/:uuid', component: ShowDecisionThreadReportComponent},	
+	{ path: 'showDecisionThreadErrors/:uuid', component: ShowDecisionThreadErrorsComponent}
 ];
 
 @NgModule({
