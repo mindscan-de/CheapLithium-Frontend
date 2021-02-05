875492f0bf970c303b2281d80d10dc37a1c6108d
mindscan-de <maxim.gansert@gmail.com>
2021-02-05 20:13:17 +0100

    simple skeleton of the show decision thread errors component.

--- src/app/show-decision-thread-errors/show-decision-thread-errors.component.ts
+++ src/app/show-decision-thread-errors/show-decision-thread-errors.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { BackendDecisionThread } from '../backend-services/backend-model/backend-decision-thread';
+
+import { DecisionThreadBackendService } from '../backend-services/decision-thread-backend.service';
 
 @Component({
   selector: 'app-show-decision-thread-errors',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-decision-thread-errors.component.css']
 })
 export class ShowDecisionThreadErrorsComponent implements OnInit {
+	
+	public decisionThread: BackendDecisionThread = new BackendDecisionThread();
+	public decisionThreadErrorItems = [];
 
-  constructor() { }
+    constructor(private activatedRoute : ActivatedRoute, private backendThreadService: DecisionThreadBackendService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		var threaduuid = this.activatedRoute.snapshot.params['uuid'];
+			
+		this.retrieveThread(threaduuid);
+	}
+
+	retrieveThread(threaduuid: string) : void {
+		this.backendThreadService.getDecisionThread(threaduuid).subscribe(
+			data => this.onDecisionThreadLoaded(data),
+			error => this.onDecisionThreadFailed(error)
+		);
+	}
+	
+	onDecisionThreadFailed(error: any) : void {
+		console.log(error);
+	}
+
+    onDecisionThreadLoaded(thread: BackendDecisionThread) : void {
+		this.decisionThread = thread;
+
+		// TODO retrieve the errors and show them...		
+    }
 
 }
